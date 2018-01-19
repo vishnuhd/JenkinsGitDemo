be28042333c0d330b4dfe27de095996a702db754
Vishnu Hari Dadhich <Vishnu.Dadhich@igglobal.com>
2018-01-19 12:17:02 +0530

    Updating JenkinsDeploy

--- JenkinsDeploy.ps1
+++ JenkinsDeploy.ps1
@@ -1,7 +1,7 @@
 #Deployment script for jenkins deployment
 
 #Backing up
-Move-Item -Path "D:\prod\Jenkins\Default\*" -Destination "D:\prod\Jenkins\Backup\*" -Force
+Move-Item -Path "D:\prod\Jenkins\Default\*" -Destination "D:\prod\Jenkins\Backup\" -Force
 
 #Copying new files
 Copy-Item -Path "D:\Jenkins\JenkinsHome\workspace\PipelineDemo\JenkinsGitDemo\bin\Release\JenkinsGitDemo.exe" -Destination "D:\prod\Jenkins\Default\"
